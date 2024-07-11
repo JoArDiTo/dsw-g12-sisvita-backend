b0c880fd606540dff2f3ce44418990fab2f81cbe
paulsantillan787 <paulggsch@gmail.com>
2024-07-11 00:29:22 -0500

    Vigilancia-Service-Discovery

--- vigilance-service/src/main/java/com/sisvita_grupo12/vigilance_service/VigilanceServiceApplication.java
+++ vigilance-service/src/main/java/com/sisvita_grupo12/vigilance_service/VigilanceServiceApplication.java
@@ -3,7 +3,9 @@ package com.sisvita_grupo12.vigilance_service;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class VigilanceServiceApplication {
 
